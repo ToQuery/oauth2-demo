d958e500fc7bc8249f3e60ad3415b04c9c63b80f
ToQuery <toquery@qq.com>
2018-10-09 11:15:08 +0800

    init project

--- oauth2-client/src/main/java/io/github/toquery/oauth2demo/Oauth2ClientApplication.java
+++ oauth2-client/src/main/java/io/github/toquery/oauth2demo/Oauth2ClientApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2DemoApplication {
+public class Oauth2ClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oauth2DemoApplication.class, args);
+        SpringApplication.run(Oauth2ClientApplication.class, args);
     }
 }
